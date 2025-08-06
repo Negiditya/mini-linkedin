1e45714a919e54b51bf8911684fe333029a67233
Negiditya <anegi1509@gmai.com>
2025-08-06 18:50:44 +0530

    Final deployment changes

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const apiUrl = process.env.REACT_APP_API_URL;
+const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+
 
 
 const instance = axios.create({
